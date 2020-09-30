fcdc0657116b4349964105d4917611cdf304923d
Shaopeng Liu <779611291@qq.com>
2020-09-29 22:37:52 -0400

    initiate WTST

--- src/try_WTST/local_test.sh
+++ src/try_WTST/local_test.sh
@@ -4,16 +4,15 @@ date
 ### workdir
 pipe_path=/Users/shaopeng/github/CMASH-reproducibles/src/try_WTST
 cd /Users/shaopeng/Desktop/WTST_test_run/test_create_ref
-
-
-conda activate /Users/shaopeng/Desktop/WTST_test_run/
-
+conda activate ${pipe_path}/py37_WTST
 
 
 ### create_ref.py var:
 query="/Users/shaopeng/Desktop/WTST_test_run/query_path.txt"
 ref="/Users/shaopeng/Desktop/WTST_test_run/ref_path.txt"
 
+
+### start to run
 
 
 
