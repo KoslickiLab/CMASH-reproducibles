63e7879fdb35e65683b83a18c57c1d57b1fca1d3
ShaopengLiu1 <sml6467@psu.edu>
2021-04-19 21:24:26 -0400

    merge Shaopeng analysis repo back

--- src/test_JI_bias_factor/manual_run.sh
+++
@@ -1,9 +0,0 @@
-# directly call the WTST env (in which the CMash has been installed)
-conda activate /Users/shaopeng/github/CMASH-reproducibles/src/try_WTST/py37_WTST
-
-# go to my local workdir
-cd /Users/shaopeng/Desktop/WTST_test_run/test_JI_bias_factor
-
-# use termianl python to run
-
-
--- src/uninstall.sh
+++ src/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###### Pipeline information:
-# v1.01, last update 07/23/2020. 
+# Last update: V1.00, 04/19/2021
 # Uninstall all dependencies to release space
 # Only need to run once
 ######
@@ -11,17 +11,6 @@
 pipe_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
 repo=$(echo ${pipe_path%/src})
 
-
-### remove conda path
-conda env remove -p ${pipe_path}/Simulation_Env_py27
-conda env remove -p ${pipe_path}/CMASH_Env_py37
-
-
-### rm CAMISIM
-rm -rf ${pipe_path}/CAMISIM
-
-### rm NCBI annotation
-rm NCBI_GenBank_*txt
 
 
 echo "All dependencies have been removed."
